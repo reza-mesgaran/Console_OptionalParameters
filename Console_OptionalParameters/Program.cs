42bb61070ee7ab9591ee93bf07e14929dc31d87f
Reza Mesgaran <reza_mesgaran@live.com>
2019-06-05 20:49:29 -0400

    Add project files.

---
+++ Console_OptionalParameters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Method
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            sum(30);   // x=30 ; y=5 ; z=10
+            sum(30, 1);  // x=30 ; y=1 ; z=10
+            sum(30, 1, 2);  // x=30 ; y=1 ; z=2
+            Console.ReadKey();
+        }
+        static void sum(int x, int y = 5, int z = 10)
+        {
+            Console.WriteLine(x + y + z);
+        }
+    }
+}
